dc6c8dc84f532f1ca477b262ad09e178bb37230a
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-09-28 21:41:21 +0800

    Add output and error checking function programme

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -35,7 +35,33 @@ def main():
             print("Error, please enter again")
 
 
+def get_valid_score():
+    score = float(input("enter your score:"))
+    while score < LOWEST_SCORE or score > HIGHEST_SCORE:
+        print("score must be between 0 and 100")
+        score = float(input("enter your score:"))
+    return score
 
+
+def print_result(score):
+    if score < LOWEST_SCORE or score > HIGHEST_SCORE:
+        print("Invalid score")
+
+    elif score > EXCELLENT_SCORE:
+        print("excellent")
+
+    elif score >= PASSABLE_SCORE:
+        print("Passable")
+
+    else:
+        print("Bad")
+
+
+def show_stars(score):
+    stars = ''
+    for i in range(int(score)):
+        stars += '*'
+    print(stars)
 
 
 main()
