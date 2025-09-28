869d30d77cedb126309a0f35ab6c0f1dec55fa33
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-09-28 21:39:21 +0800

    Add function menu programme

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -3,3 +3,39 @@ HIGHEST_SCORE = 100
 EXCELLENT_SCORE = 90
 PASSABLE_SCORE = 50
 
+def main():
+    score = LOWEST_SCORE
+    choice = ''
+    while choice != 'Q':
+        print("(G)et a valid score")
+        print("(P)rint result")
+        print("(S)how stars")
+        print("(Q)uit")
+        choice = input("Enter your choice:").upper()
+
+        if choice == 'G':
+            score = get_valid_score()
+
+        elif choice == 'P':
+            if score == LOWEST_SCORE:
+                print("Please get a score first!")
+            else:
+                print_result(score)
+
+        elif choice == 'S':
+            if score == LOWEST_SCORE:
+                print("Please get a score first!")
+            else:
+                show_stars(score)
+
+        elif choice == 'Q':
+            print("farewell")
+
+        else:
+            print("Error, please enter again")
+
+
+
+
+
+main()
