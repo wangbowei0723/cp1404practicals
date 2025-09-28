0e3f45bb23849734f497f397028a9c326453bfeb
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-09-28 21:02:57 +0800

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,15 +4,18 @@ def main():
     password = get_password()
     print_asterisks(password)
 
+
 def print_asterisks(password):
     for i in range(len(password)):
         print("*", end="")
+
 
 def get_password():
     password = input("enter password:")
     while len(password) < minimum_length:
         print("password must be at least 7 characters")
         password = input("enter password:")
-    return password
+    return password:
+
 
 main()
