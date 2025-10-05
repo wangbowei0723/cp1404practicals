e81ae738482cbbaed0091f2e2083b6186e07884a
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-05 20:35:47 +0800

    Finish all the task

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+"""
+Line 1: A random integer between 5 and 20.
+Smallest: 5, Largest: 20
+
+
+Line 2: A random odd integer from 3, 5, 7, 9.
+Smallest: 3, Largest: 9
+No, cannot produce 4, because the step is 2 and the start is 3).
+
+
+Line 3: A random float >=2.5 and <5.5.
+Smallest: 2.5, Largest: close to 5.5
+"""
+
+import random
+print(random.randint(1, 100))
