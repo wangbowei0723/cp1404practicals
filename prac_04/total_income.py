f57df60814433f5bdecdf9a2b1a9339c563d906d
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-12 18:45:52 +0800

    Add user input to get the number of months

---
+++ prac_04/total_income.py
@@ -0,0 +1,14 @@
+"""
+CP1404/CP5632 Practical
+Refactored cumulative total income program
+"""
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    number_of_months = int(input("How many months? "))
+
+
+
+main()
+
