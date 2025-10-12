26367a631fa6bf38054976a8e2853004365edd88
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-12 19:08:40 +0800

    Add list of valid usernames

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,3 +12,9 @@ def main():
 
 main()
 
+def main():
+# Security checker
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+                 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+                 'Command', 'ExecState', 'InteractiveConsole',
+                 'InterpreterInterface', 'StartServer', 'bob']
