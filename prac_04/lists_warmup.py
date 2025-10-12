65dd3d94f58faf271773fe56e072f95b097a84e8
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-12 18:33:24 +0800

    Complete all questions of this file

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Predict the value of the following (before running):
+# numbers[0]               answer: 3
+# numbers[-1]              answer: 2
+# numbers[3]               answer: 1
+# numbers[:-1]             answer: [3, 1, 4, 1, 5, 9]
+# numbers[3:4]             answer: [1]
+# 5 in numbers             answer: True
+# 7 in numbers             answer: False
+# "3" in numbers           answer: False (because it's a string)
+# numbers + [6, 5, 3]      answer: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
+
+
