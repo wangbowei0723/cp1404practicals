ed5f5da6bc3a80593ecb5c784fde0c017ce82ce7
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-12 18:48:11 +0800

    Add loop to collect income for each month

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -8,7 +8,9 @@ def main():
     incomes = []
     number_of_months = int(input("How many months? "))
 
-
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
 
 main()
 
