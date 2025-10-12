8430bf1f389b70a1130b0718e434f1012976d706
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-12 19:05:28 +0800

    Add average calculation and display result

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -8,7 +8,7 @@ def main():
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
-
+    print(f"The average of the numbers is {sum(numbers) / len(numbers):.1f}")
 
 main()
 
