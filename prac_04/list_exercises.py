72994e0eaa22aa1b8b57153e9b3376da23a78f06
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-12 19:04:14 +0800

    Add print statements for first and last numbers

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,6 +4,9 @@ def main():
         number = int(input("Number: "))
         numbers.append(number)
 
+    print(f"The first number is {numbers[0]}")
+    print(f"The last number is {numbers[-1]}")
+
 
 main()
 
