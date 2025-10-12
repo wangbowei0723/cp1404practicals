2c7ed1f99324305b2ff0a2016d642a3c052c5b08
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-10-12 18:56:10 +0800

    Implement load_data function to read and process file data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,5 +8,16 @@ FILENAME = "subject_data.txt"
 def main():
     """Load data from file and display subject information."""
 
+def load_data():
+    """Read data from file formatted """
+    subject_data = []
+    input_file = open(FILENAME)
+    for line in input_file:
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])  # Convert student count to int
+        subject_data.append(parts)
+    input_file.close()
+    return subject_data
 
 main()
