f73e4d09a6923359649a89f7fe48ddc073405a4c
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-11-09 22:12:52 +0800

    add project class

---
+++ prac_07/project.py
@@ -0,0 +1,17 @@
+"""
+project
+Estimate: 30 minutes
+Actual: 35 minutes
+"""
+
+from datetime import datetime
+
+
+class Project:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
