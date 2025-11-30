76bcf00a6a7b8dbbd7cb4f3b71feeda53eaadf35
WBOY <BOWEI.WANG@MY.JCU.EDU.AU>
2025-11-30 14:26:17 +0800

    Make random number and compare

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,3 +4,15 @@ from random import randint
 class UnreliableCar(Car):
     """Represent a Car object."""
 
+    def __init__(self, name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """generate a random number, drive the car if number < reliability"""
+        random_number = randint(0, 100)
+        if random_number < self.reliability:
+            return 0
+
+        drive_distance = super().drive(distance)
+        return drive_distance
